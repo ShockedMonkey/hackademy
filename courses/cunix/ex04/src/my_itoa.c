f887baf4bbb08b21eb543e070a1526d0849e5570
Ilias Suleimanov <ae_isuleimanov@indeed.com>
2021-08-08 21:41:33 +0300

    implement itoa, fix style

---
+++ courses/cunix/ex04/src/my_itoa.c
@@ -0,0 +1,36 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+#define BUFF_SIZE 12 // as 12 bytes is max for integer repr
+
+void reverse(char *target, int len)
+{
+    for (int i = 0; i < len / 2; i++)
+    {
+        int tmp = target[i];
+        target[i] = target[len - i - 1];
+        target[len - i - 1] = tmp;
+    }
+}
+
+char *my_itoa(int nbr)
+{
+    int len = 0;
+    char *buff = malloc(sizeof(char) * BUFF_SIZE);;
+    int sign = nbr >= 0 ? 1 : -1;
+    nbr *= sign;
+    do
+    {
+        buff[len++] = nbr % 10 + '0';
+        nbr = nbr / 10;
+    }
+    while (nbr > 0);
+
+    if (sign == -1)
+    {
+        buff[len++] = '-';
+    }
+    buff[len] = '\0';
+    reverse(buff, len);
+    return buff;
+}
